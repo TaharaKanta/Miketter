f8480ff2e198c1037d516b9775c1ea0a88d767bd
TaharaKanta <piraruku66@gmail.com>
2016-06-21 20:50:47 +0900

    first commit(MA)

---
+++ app/src/main/java/jp/co/spookies/android/miketter/MiketterActivity.java
@@ -0,0 +1,71 @@
+package jp.co.spookies.android.miketter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.speech.RecognizerIntent;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MiketterActivity extends Activity {
+	private final int REQUEST_CODE_MIC = 502;
+	private final int MIC_CAND_SIZE = 5;
+	private Twitter twitter = new TwitterFactory().getInstance();
+	private Handler handler = new Handler();
+	private String message = "";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		setContentView(R.layout.main);
+		twitter.setOAuthConsumer(getString(R.string.consumer_key),
+				getString(R.string.consumer_secret));
+	}
+
+	public void onReloadButtonClicked(View view) {
+		try {
+			reload();
+		} catch (TwitterException e) {
+			showErrorDialog();
+		}
+	}
